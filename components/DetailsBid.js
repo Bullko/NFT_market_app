f19d3a20147fb401b24ebd193c4ba7650d108939
Bullko <krajnak.anton@gmail.com>
2023-10-12 13:12:13 +0200

    completed

---
+++ components/DetailsBid.js
@@ -0,0 +1,48 @@
+import { View, Text, Image } from "react-native";
+import { EthPrice } from "./SubInfo";
+import { COLORS, FONTS, SIZES, SHADOWS, assets } from "../constants";
+
+const DetailsBid = ({ bid }) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginVertical: SIZES.base,
+        paddingHorizontal: SIZES.base * 2,
+      }}
+    >
+      <Image
+        source={bid.image}
+        resizeMode="contain"
+        style={{ width: 48, height: 48 }}
+      />
+      <View>
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}
+        >
+          Bid placed by {bid.name}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 3,
+            color: COLORS.secondary,
+            marginTop: 3,
+          }}
+        >
+          {bid.date}
+        </Text>
+      </View>
+      <EthPrice price={bid.price} />
+    </View>
+  );
+};
+
+export default DetailsBid;
